b0245356facd8d1142bcf627bd829a51678915c9
ZeframLou <zeframlou@gmail.com>
2023-05-31 20:52:39 -0700

    feat: add basic tests

--- test/SharedPool.t.sol
+++ test/SharedPool.t.sol
@@ -1,14 +1,131 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
 
-contract SharedPoolTest is Test {
-    function setUp() public {}
+import {RoyaltyEngine} from "lssvm2/RoyaltyEngine.sol";
+import {XykCurve} from "lssvm2/bonding-curves/XykCurve.sol";
+import {LSSVMPair, LSSVMPairFactory} from "lssvm2/LSSVMPairFactory.sol";
+import {LSSVMPairETH} from "lssvm2/LSSVMPairETH.sol";
+import {LSSVMPairERC721ETH} from "lssvm2/erc721/LSSVMPairERC721ETH.sol";
+import {LSSVMPairERC1155ETH} from "lssvm2/erc1155/LSSVMPairERC1155ETH.sol";
+import {LSSVMPairERC721ERC20} from "lssvm2/erc721/LSSVMPairERC721ERC20.sol";
+import {LSSVMPairERC1155ERC20} from "lssvm2/erc1155/LSSVMPairERC1155ERC20.sol";
 
-    function testExample() public {
-        vm.startPrank(address(0xB0B));
-        console2.log("Hello world!");
-        assertTrue(true);
+import {RoyaltyRegistry} from "manifoldxyz/RoyaltyRegistry.sol";
+
+import "../src/SharedPoolFactory.sol";
+import {TestERC20} from "./mocks/TestERC20.sol";
+import {TestERC721} from "./mocks/TestERC721.sol";
+import {TestERC1155} from "./mocks/TestERC1155.sol";
+
+contract SharedPoolTest is Test {
+    uint256 constant PROTOCOL_FEE = 0.005e18;
+    address payable constant PROTOCOL_FEE_RECIPIENT = payable(address(0xfee));
+
+    SharedPoolFactory factory;
+    LSSVMPairFactory pairFactory;
+    XykCurve bondingCurve;
+    TestERC20 testERC20;
+    TestERC721 testERC721;
+    TestERC1155 testERC1155;
+
+    function setUp() public {
+        // deploy LSSVMPairFactory
+        RoyaltyRegistry royaltyRegistry = new RoyaltyRegistry(address(0));
+        royaltyRegistry.initialize(address(this));
+        RoyaltyEngine royaltyEngine = new RoyaltyEngine(address(royaltyRegistry));
+        LSSVMPairERC721ETH erc721ETHTemplate = new LSSVMPairERC721ETH(royaltyEngine);
+        LSSVMPairERC721ERC20 erc721ERC20Template = new LSSVMPairERC721ERC20(royaltyEngine);
+        LSSVMPairERC1155ETH erc1155ETHTemplate = new LSSVMPairERC1155ETH(royaltyEngine);
+        LSSVMPairERC1155ERC20 erc1155ERC20Template = new LSSVMPairERC1155ERC20(royaltyEngine);
+        pairFactory = new LSSVMPairFactory(
+            erc721ETHTemplate,
+            erc721ERC20Template,
+            erc1155ETHTemplate,
+            erc1155ERC20Template,
+            PROTOCOL_FEE_RECIPIENT,
+            PROTOCOL_FEE,
+            address(this)
+        );
+        bondingCurve = new XykCurve();
+        pairFactory.setBondingCurveAllowed(bondingCurve, true);
+
+        // deploy shared pool factory
+        factory = new SharedPoolFactory(
+            new SharedPoolERC721ETH(),
+            new SharedPoolERC721ERC20(),
+            new SharedPoolERC1155ETH(),
+            new SharedPoolERC1155ERC20(),
+            pairFactory,
+            bondingCurve
+        );
+
+        // deploy test tokens
+        testERC20 = new TestERC20();
+        testERC721 = new TestERC721();
+        testERC1155 = new TestERC1155();
     }
+
+    function test_createSharedPoolERC721ETH(uint256 delta, uint256 spotPrice, uint256 fee) public {
+        delta = bound(delta, 0, 1000);
+        spotPrice = bound(spotPrice, 0, 1e20);
+        fee = bound(fee, 0, 1e17);
+
+        SharedPoolERC721ETH pool =
+            factory.createSharedPoolERC721ETH(testERC721, uint128(delta), uint128(spotPrice), uint96(fee), address(0));
+        assertEq(pool.pair().delta(), delta);
+        assertEq(pool.pair().spotPrice(), spotPrice);
+        assertEq(pool.pair().fee(), fee);
+        assertEq(pool.initialDelta(), delta);
+        assertEq(pool.initialSpotPrice(), spotPrice);
+        assertEq(pool.nft(), address(testERC721));
+        assertEq(address(pool.pairFactory()), address(pairFactory));
+        assertEq(address(pool.token()), address(0));
+    }
+
+    function test_deposit(uint256 delta, uint256 spotPrice, uint256 fee, uint256 numNfts, uint256 tokenAmount) public {
+        delta = bound(delta, 0, 1000);
+        spotPrice = bound(spotPrice, 0, 1e20);
+        fee = bound(fee, 0, 1e17);
+        numNfts = bound(numNfts, 1, 10);
+        tokenAmount = bound(tokenAmount, 1, 1e20);
+
+        // deploy pool
+        SharedPoolERC721ETH pool =
+            factory.createSharedPoolERC721ETH(testERC721, uint128(delta), uint128(spotPrice), uint96(fee), address(0));
+
+        // mint NFTs
+        testERC721.setApprovalForAll(address(pool), true);
+        uint256[] memory idList = _getIdList(1, numNfts);
+        for (uint256 i; i < numNfts; i++) {
+            testERC721.safeMint(address(this), idList[i]);
+        }
+
+        // deposit
+        deal(address(this), tokenAmount);
+        uint256 liquidity = pool.deposit{value: tokenAmount}(idList, 0, address(this), bytes(""));
+        assertGt(liquidity, 0, "minted 0 liquidity");
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC721 compliance
+    /// -----------------------------------------------------------------------
+
+    function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Utilities
+    /// -----------------------------------------------------------------------
+
+    function _getIdList(uint256 startId, uint256 amount) internal pure returns (uint256[] memory idList) {
+        idList = new uint256[](amount);
+        for (uint256 i = startId; i < startId + amount; i++) {
+            idList[i - startId] = i;
+        }
+    }
+
+    receive() external payable {}
 }
---
+++ test/mocks/TestERC1155.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import {ERC1155} from "solmate/tokens/ERC1155.sol";
+
+contract TestERC1155 is ERC1155 {
+    function uri(uint256) public view virtual override returns (string memory) {
+        return "";
+    }
+
+    function mint(address to, uint256 id, uint256 amount) public {
+        _mint(to, id, amount, "");
+    }
+}
