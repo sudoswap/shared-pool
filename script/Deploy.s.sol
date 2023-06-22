3a7dc273b649dadc950598b9aae059582471e4c0
ZeframLou <zeframlou@gmail.com>
2023-06-21 18:33:02 -0700

    feat: add deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {CREATE3Script} from "./base/CREATE3Script.sol";
-import {SharedPoolFactory} from "../src/SharedPoolFactory.sol";
+import "../src/SharedPoolFactory.sol";
 
 contract DeployScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
@@ -10,14 +10,25 @@ contract DeployScript is CREATE3Script {
     function run() external returns (SharedPoolFactory c) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        uint256 param = 123;
+        address pairFactory = vm.envAddress("PAIR_FACTORY");
+        address xykCurve = vm.envAddress("XYK_CURVE");
 
         vm.startBroadcast(deployerPrivateKey);
 
         c = SharedPoolFactory(
             create3.deploy(
                 getCreate3ContractSalt("SharedPoolFactory"),
-                bytes.concat(type(SharedPoolFactory).creationCode, abi.encode(param))
+                bytes.concat(
+                    type(SharedPoolFactory).creationCode,
+                    abi.encode(
+                        new SharedPoolERC721ETH(),
+                        new SharedPoolERC721ERC20(),
+                        new SharedPoolERC1155ETH(),
+                        new SharedPoolERC1155ERC20(),
+                        pairFactory,
+                        xykCurve
+                    )
+                )
             )
         );
 
