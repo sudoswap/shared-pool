a688ce0b292988c45c1ca869c3ea12ed8ffa0d4d
ZeframLou <zeframlou@gmail.com>
2023-07-24 16:34:25 -0700

    build: update deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,20 +2,25 @@
 pragma solidity ^0.8.13;
 
 import {CREATE3Script} from "./base/CREATE3Script.sol";
+
+import {Splitter} from "lssvm2/settings/Splitter.sol";
+
 import "../src/SharedPoolFactory.sol";
+import "../src/settings/SplitSettingsFactory.sol";
 
 contract DeployScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() external returns (SharedPoolFactory c) {
+    function run() external returns (SharedPoolFactory sharedPoolFactory, SplitSettingsFactory splitSettingsFactory) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        address pairFactory = vm.envAddress("PAIR_FACTORY");
+        LSSVMPairFactory pairFactory = LSSVMPairFactory(payable(vm.envAddress("PAIR_FACTORY")));
         address xykCurve = vm.envAddress("XYK_CURVE");
+        Splitter splitterImpl = Splitter(payable(vm.envAddress("SPLITTER_IMPL")));
 
         vm.startBroadcast(deployerPrivateKey);
 
-        c = SharedPoolFactory(
+        sharedPoolFactory = SharedPoolFactory(
             create3.deploy(
                 getCreate3ContractSalt("SharedPoolFactory"),
                 bytes.concat(
@@ -28,6 +33,16 @@ contract DeployScript is CREATE3Script {
                         pairFactory,
                         xykCurve
                     )
+                )
+            )
+        );
+
+        splitSettingsFactory = SplitSettingsFactory(
+            create3.deploy(
+                getCreate3ContractSalt("SplitSettingsFactory"),
+                bytes.concat(
+                    type(SplitSettingsFactory).creationCode,
+                    abi.encode(new SplitSettings(splitterImpl, LSSVMPairFactory(pairFactory)))
                 )
             )
         );
